e376eb495056f4f67c16b88316fb8b64acc9c0c4
julian <julianpitney@gmail.com>
2019-02-11 14:37:54 -0500

    Adding existence check so data doesn't get deleted

--- raspivid_record.sh
+++ raspivid_record.sh
@@ -2,6 +2,13 @@
 
 read -p "Enter video title: " vidTitle
 
+if [ -e /home/pi/sarahsVideos/$vidTitle".h264" ]
+then
+	echo "File already exists! Aborting!"
+	exit	
+fi
+
+
 raspivid --mode 4 -t 0 --preview '0,0,640,480' --opacity 240 --framerate 30 --exposure fixedfps --shutter 1000 -o /home/pi/sarahsVideos/$vidTitle".h264"
 
 
